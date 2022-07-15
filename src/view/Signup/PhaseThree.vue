1ff463ea7dbee8450446bb1f10b1921e66dd5e81
Bill1907 <a31829660@gamil.com>
2022-07-15 23:10:44 +0900

    unit card input 4자리수 숫자만 입력

--- src/view/Signup/PhaseThree.vue
+++ src/view/Signup/PhaseThree.vue
@@ -3,16 +3,40 @@
     <span>카드번호</span>
     <div>
       <label for="card1">
-        <input id="card1" type="number">
+        <input
+          :value="card1"
+          id="card1"
+          type="text"
+          maxlength="4"
+          @input="validateCardUnitNum($event, 'card1')"
+        >
       </label>
       <label for="card2">
-        <input id="card2" type="number">
+        <input
+          :value="card2"
+          id="card2"
+          type="text"
+          maxlength="4"
+          @input="validateCardUnitNum($event, 'card2')"
+        >
       </label>
       <label for="card3">
-        <input id="card3" type="number">
+        <input
+          :value="card3"
+          id="card3"
+          type="text"
+          maxlength="4"
+          @input="validateCardUnitNum($event, 'card3')"
+        >
       </label>
-      <label for="card4">
-      <input id="card4" type="number">
+      <label for="card4" @focusout="handleFocusoutCardNum">
+      <input
+        :value="card4"
+        id="card4"
+        type="text"
+        maxlength="4"
+        @input="validateCardUnitNum($event, 'card4')"
+      >
       </label>
     </div>
     <div>
@@ -24,6 +48,31 @@
 <script>
 export default {
   name: 'PhaseThree',
+  data() {
+    return {
+      card1: '',
+      card2: '',
+      card3: '',
+      card4: '',
+    };
+  },
+  methods: {
+    handleFocusoutCardNum() {
+      this.validateCardNum();
+    },
+    validateCardNum() {
+      const cardNum = this.card1 + this.card2 + this.card3 + this.card4;
+      console.log(cardNum);
+    },
+    validateCardUnitNum(event, card) {
+      event.target.value = event.target.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+      if (event.target.value.length === 4) {
+        this[card] = event.target.value;
+      } else {
+        console.log('not enough');
+      }
+    },
+  },
 };
 </script>
 
